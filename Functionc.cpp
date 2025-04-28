9243e7a19d148abf8629b3f70c7408686f20a777
PrakritiSahu <154116914+PrakritiSahu@users.noreply.github.com>
2025-04-28 19:42:55 +0530

    Add files via upload

---
+++ Functionc.cpp
@@ -0,0 +1,85 @@
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n=3;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=1; i<=4; i++){
+        for(int j=1; j<=i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=1; i<=5; i++){
+        for(int j=1; j<=i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}*/
+
+
+//second method
+/*#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    for(int i=1; i<=a; i++){
+        for(int j=1; j<=i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=1; i<=b; i++){
+        for(int j=1; j<=i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=1; i<=c; i++){
+        for(int j=1; j<=i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}*/
+
+
+//third method
+/*#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    for(int k=1; k<=3; k++){
+        cin>>a;
+        for(int i=1; i<=a; i++){
+            for(int j=1; j<=i; j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+    }
+}*/
+
+
+
+//fourth method
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    for(int k=1; k<=3; k++){
+        cin>>a;
+        for(int i=1; i<=a; i++){
+            for(int j=1; j<=i; j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+    }
+}
